1fa3292ca537ebb1a022be3d7d8e53a3bf0eac3b
ramonornela <ramon.ornela@gmail.com>
2017-05-10 16:38:59 -0300

    fix(module): import provider Network @ionic-native 3.x

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HttpPluginsToken } from '@mbamobi/http';
+import { Network } from '@ionic-native/network';
 import { IonicModule, LoadingController } from 'ionic-angular';
 import { COMPONENTS } from './components';
 import { LoadingIonicPlugin, NoConnectionPlugin } from './providers';
@@ -10,7 +11,8 @@ import { LoadingIonicPlugin, NoConnectionPlugin } from './providers';
   declarations: [ COMPONENTS ],
   providers: [
     { provide: HttpPluginsToken, useClass: LoadingIonicPlugin, deps: [ LoadingController ], multi: true },
-    { provide: HttpPluginsToken, useClass: NoConnectionPlugin, multi: true }
+    { provide: HttpPluginsToken, useClass: NoConnectionPlugin, multi: true },
+    Network
   ]
 })
 export class HttpPluginsIonicModule {
